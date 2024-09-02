ae6457691a22760abd356a0855a7f46f310d2f81
Hasona <bekoahmed20@gmail.com>
2024-09-02 19:16:05 +0300

    finished!

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-import express from "express";
-import AppController from '../controllers/AppController.js'
-import UsersController from '../controllers/UsersController.js'
-import AuthController from '../controllers/AuthController.js'
-import FilesController from '../controllers/FilesController.js'
+import express from 'express';
+import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
+import FilesController from '../controllers/FilesController.js';
 
 const router = express.Router();
 
@@ -11,9 +11,9 @@ router.get('/stats', (req, res) => AppController.getStats(req, res));
 
 router.post('/users', (req, res) => UsersController.postNew(req, res));
 
-router.get('/connect', (req , res) => AuthController.getConnect(req, res));
-router.get('/disconnect', (req , res) => AuthController.getDisconnect(req, res));
-router.get('/users/me', (req , res) => AuthController.getMe(req, res));
+router.get('/connect', (req, res) => AuthController.getConnect(req, res));
+router.get('/disconnect', (req, res) => AuthController.getDisconnect(req, res));
+router.get('/users/me', (req, res) => AuthController.getMe(req, res));
 
 router.post('/files', (req, res) => FilesController.postUpload(req, res));
 
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import router from './routes/index.js'
-
+import router from './routes/index.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.listen(port, '0.0.0.0',() => {
-    console.log(`Server running on port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
